0c4545a4112cc18618e3d0fce704eca18ccb55d2
prizely <19091@chw.edu.hk>
2024-10-06 23:01:30 +0800

    main

--- React_default_setup/src/index.js
+++ React_default_setup/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Main from './Main';
 
+const myfirstelement = <h1>YO</h1>
+
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(<Main />);

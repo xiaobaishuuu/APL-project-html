441a39809307d9361d35a1a03fc0dbe79e636a4a
xiaobaishuuu <xbs.mc01@gmail.com>
2024-10-20 19:58:57 +0800

    change

--- project-app/src/index.js
+++ project-app/src/index.js
@@ -1,13 +1,20 @@
 import ReactDOM from "react-dom"
 import React from "react"
+import './styles.css'
+
+function Header(){
+    return (<h1>🍉物件配对✈️</h1>)
+}
 
 class Weather extends React.Component{
-  state = {isHot:true}
-  render(){
-      return <h1 onClick={this.changeWeather}>today  {this.state.isHot? 'Hot':'cold'}</h1>
-  }
-  changeWeather =()=>{
-      this.setState({isHot:!this.state.isHot})
-  }
+    state = {isHot:true}
+    render(){
+        return <h1 onClick={this.changeWeather}>today  {this.state.isHot? 'Hot':'cold'}</h1>
+    }
+    changeWeather =()=>{
+        this.setState({isHot:!this.state.isHot})
+    }
 }
-ReactDOM.render(<Weather/>,document.getElementById('test'))
+
+ReactDOM.render(<Header/>,document.getElementById('head'))
+// ReactDOM.render(<Header/>,document.getElementsByTagName('header'))

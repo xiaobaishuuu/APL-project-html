059b3ebfcd3d8e067897bccfdc8661f092db50f2
xiaobaishuuu <117255248+xiaobaishuuu@users.noreply.github.com>
2024-10-26 12:55:06 +0800

    change

--- project-app/src/components/Game/Game.jsx
+++ project-app/src/components/Game/Game.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import {nanoid} from 'nanoid'
 import './Game.css'
 
@@ -16,8 +16,6 @@ class ChoiceDifficult extends React.Component{
             case this.hard:
                 column = 8
                 break;
-            default:
-                console.log(1)
         }
         this.props.diffcult(column)
     }
@@ -27,45 +25,54 @@ class ChoiceDifficult extends React.Component{
                     <button ref={current => this.easy = current}   onClick={this.difficultHandle}>簡單模式</button>
                     <button ref={current => this.normal = current} onClick={this.difficultHandle}>正常模式</button>
                     <button ref={current => this.hard = current}   onClick={this.difficultHandle}>困難模式</button>
-                    {/* <button>12</button>
-                    <button>12s</button>
-                    <input type="text"/>
-                    <button>submit</button> */}
                 </div>
     }
 }
 
 class Card extends React.Component{
     state = {show:false}
-    filpCard = () =>{
+    filpCard = (event) =>{
+        if (this.props.match) return
+        console.log(event.target)
+        this.props.cardGetter(event.target)
         this.setState({show:!this.state.show})
     }
     render(){
-        return (<div className="card" onClick={this.filpCard}>
-                    <span>{this.state.show? this.props.emoji:''}</span>
+        return (<div className='card'  onClick={this.filpCard}>
+                    <span id={this.props.id}>{this.state.show? this.props.emoji:''}</span>
                 </div>)
     }
 }
 
 export default class Game extends React.Component{
     state = {card:[],
-            clickOne: null,
-            clickTwo: null
+        clickCards:[]
+    }
+
+    cardGetter = (event) =>{
+        const {clickCards} = this.state
+        let newClickCards = clickCards
+        if (clickCards.length < 2){
+            newClickCards  = [event.target.innerText,...clickCards];
+        }else if(clickCards[0] == clickCards[1]){
+            
+        }
+        this.setState({clickCards:newClickCards})
     }
 
     checkCard = () =>{
         let {clickOne,clickTwo} = this.state
-        if (clickOne == clickTwo){
+        if (clickOne === clickTwo){
             [clickOne,clickTwo] = null
-            // console.log(clickOne.clickTwo)
         }
     }
 
     diffcultGetter = (column) =>{
         let newState = {card:[]}
         for (let i = 0; i < column*2; i++) {
-            newState.card.push({id:nanoid(),emoji:String.fromCodePoint(0x1f601 + i)})
-            newState.card.push({id:nanoid(),emoji:String.fromCodePoint(0x1f601 + i)})
+            for(let j = 0;j < 2;j++){
+                newState.card.push({id:nanoid(),emoji:String.fromCodePoint(0x1f601 + i),match:false})
+            }
         }
         this.setState(newState)
     }
@@ -74,7 +81,7 @@ export default class Game extends React.Component{
                     <ChoiceDifficult diffcult={this.diffcultGetter}/>
                     <div className="cards-container">{
                         this.state.card.map(card =>{
-                            return <Card key={card.id} {...card}/>
+                            return <Card key={card.id} cardGetter={this.cardGetter} {...card}/>
                         })}
                         </div>
                 </main>)

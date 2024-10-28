69cc437a2d4a4ef6fbc9e1541ed0f19f6a8ed520
xiaobaishuuu <xbs.mc01@gmail.com>
2024-10-28 19:47:09 +0800

    chaneg

--- project-app/src/components/Footer/Footer.js
+++ project-app/src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './Footer.css'
 
+//this is an easy part,just do it okay? only html, no need in jsx
 export default function Footer(){
     return <div></div>
 }
--- project-app/src/components/Game/Game.jsx
+++ project-app/src/components/Game/Game.jsx
@@ -3,6 +3,9 @@ import {nanoid} from 'nanoid';
 import './Game.css';
 
 const ChoiceDifficult = ({setDifficult}) => {
+    // write a difficult and theme changer -- use input element
+    // diffcult limitation is 1-100,
+    // theme can be country flag or foot .etc  just mark down the unicode here,or change direactly in function(setDeck)
     return (
         <div className="choiceDifficult">
             <button onClick={() => setDifficult(4)}>簡單模式</button>
@@ -11,58 +14,49 @@ const ChoiceDifficult = ({setDifficult}) => {
         </div>
     );
 };
-
 const Card = (props) => {
-    const setClass = () =>{
-        let classname = 'card'
-        if (props.isFlipped || props.isMatched) classname += ' flipped'
-        if (props.isMatched) classname += ' matched'
-        return classname
+    const getClass = () =>{
+        let newClass = 'card';
+        if (props.isFlipped || props.isMatched) newClass += ' flipped';
+        if (props.isMatched) newClass += ' matched'
+        return newClass
     }
     return (
-        <div className={setClass()}  onClick={props.onClick}>
+        <div className={getClass()}  onClick={props.onClick}>
             <span className="card-back">{props.emoji}</span>
         </div>
     )
 };
-
-const setDeck = (columns) =>{
+const setDeck = (columns,theme = '') =>{       // write a shuffle func for (newCards) just add in setDeck
     let newCards = [];
     for (let i = 0; i < columns*2; i++) {
         const emoji = String.fromCodePoint(0x1f601 + i);
-        newCards.push({key:nanoid(),emoji});
-        newCards.push({key:nanoid(),emoji});
+        newCards.push({key:nanoid(),emoji},{key:nanoid(),emoji});
     }
-    return newCards.sort(() =>  - 0.5)
+    return newCards
 }
-
 export default function Game(){
     const [cards, setCards] = useState(setDeck(5));
     const [flippedCards, setFlippedCards] = useState([]);
     const [matchedEmojis, setMatchedEmojis] = useState([]);
 
     const difficultyGetter = (columns) => {
-        setCards(setDeck(columns));
+        setCards(setDeck(columns))
         setFlippedCards([])
         setMatchedEmojis([])
-    };
-
-    const handleCardClick = (cardObj) => {
-        if (flippedCards.length < 2 && !matchedEmojis.includes(cardObj.emoji)) {
+    }
+    const checkCard = (cardObj) => {
+        if (flippedCards.length < 2 && !matchedEmojis.includes(cardObj.emoji) && !flippedCards.includes(cardObj)) {
             const newFlippedCards = [cardObj,...flippedCards];
-            setFlippedCards(newFlippedCards);
+            setFlippedCards(newFlippedCards)
             if (newFlippedCards.length === 2) {
-                setTimeout(() => {checkForMatch(newFlippedCards);}, 500);
+                setTimeout(() => {
+                if (newFlippedCards[0].emoji === newFlippedCards[1].emoji) {
+                    setMatchedEmojis((emojis) => [newFlippedCards[0].emoji,...emojis])
+                }setFlippedCards([])}, 500)
             }
         }
-    };
-
-    const checkForMatch = (flippedCards) => {
-        if (flippedCards[0].emoji === flippedCards[1].emoji) {
-            setMatchedEmojis((emojis) => [flippedCards[0].emoji,...emojis]);
-        }
-        setFlippedCards([]);
-    };
+    }
     return (
         <main>
             <ChoiceDifficult setDifficult={difficultyGetter} />
@@ -71,9 +65,10 @@ export default function Game(){
                     <Card {...card}
                     isMatched={matchedEmojis.includes(card.emoji)}
                     isFlipped={flippedCards.includes(card)}
-                    onClick={() => handleCardClick(card)}/>
+                    flippedCards = {flippedCards}
+                    onClick={() => checkCard(card)}/>
                 ))}
             </div>
         </main>
-    );
-};
+    )
+}
